3b411befdf13294862bbe31df7123dbb9b9b6bfb
Erin Carroll <erin_carroll@berkeley.edu>
2026-04-25 15:00:31 -0700

    extract spectra

--- granule_metadata.py
+++ granule_metadata.py
@@ -3,6 +3,7 @@ import pandas as pd
 import h5py
 import numpy as np
 from glob import glob
+from tqdm import tqdm
 
 # 2018
 granule_ids = []
@@ -15,23 +16,29 @@ cloud_types = []
 gsd = 1
 raster_epsg = 32613
 flightline_ids = []
-fps = glob('/store/carroll/col/data/2018/raw/L1/radianceH5/*.h5')
-for fp in fps:
-    with h5py.File(fp, "r") as f:
-        time = f['CRBU/Radiance'].attrs['Acquisition_Time']
-        cloud_condition = f['CRBU/Radiance/Radiance_Data'].attrs['Cloud conditions']
-        cloud_type = f['CRBU/Radiance/Radiance_Data'].attrs['Cloud type']
-    acquistion_date = time.split(',')[0]
-    acquistion_start_time = time.split(',')[1].replace('[Computer Time in sec]', '').strip()
-    granule_id = f'NIS01_{acquistion_date.replace("-", "")}_{acquistion_start_time}'
-    flightline_id = granule_id
-    cloud_condition = cloud_condition.split(' (')[0]
 
-    granule_ids.append(granule_id)
-    acquistion_dates.append(acquistion_date)
-    acquistion_start_times.append(acquistion_start_time)
-    cloudy_conditions.append(cloud_condition)
-    cloud_types.append(cloud_type)
+fps = glob('/store/carroll/col/data/2018/raw/L1/radianceH5/*/*.h5')
+
+for fp in fps:
+    try:
+        with h5py.File(fp, "r") as f:
+            time = f['CRBU/Radiance'].attrs['Acquisition_Time']
+            cloud_condition = f['CRBU/Radiance/Radiance_Data'].attrs['Cloud conditions']
+            cloud_type = f['CRBU/Radiance/Radiance_Data'].attrs['Cloud type']
+        acquistion_date = time.split(',')[0]
+        acquistion_start_time = time.split(',')[1].replace('[Computer Time in sec]', '').strip()
+        granule_id = f'NIS01_{acquistion_date.replace("-", "")}_{acquistion_start_time}'
+        flightline_id = granule_id
+        cloud_condition = cloud_condition.split(' (')[0]
+
+        granule_ids.append(granule_id)
+        acquistion_dates.append(acquistion_date)
+        acquistion_start_times.append(acquistion_start_time)
+        cloudy_conditions.append(cloud_condition)
+        cloud_types.append(cloud_type)
+    except Exception as e:
+        print(fp)
+
 df = pd.DataFrame({
     'granule_id': granule_ids, 
     'campaign_name': campaign_name,
@@ -46,48 +53,50 @@ df = pd.DataFrame({
     'granule_rad_url': [None] * len(granule_ids),
     'granule_refl_url': [None] * len(granule_ids)
 })
-df.to_csv('out/2018/granule_metadata.csv', index=False)
+df.to_csv('/store/carroll/sbgplants/out/granule_metadata.csv', index=False)
+print(df.shape)
+print('exported')
 
-# 2025
-granule_ids = []
-campaign_name = 'Colorado Headwaters Ecological Spectroscopy Study'
-sensor_name = 'NEON AIS 1'
-acquistion_dates = []
-acquistion_start_times = []
-cloudy_conditions = []
-cloud_types = []
-gsd = 1
-raster_epsg = 32613
-flightline_ids = []
-fps = glob('/store/carroll/col/data/2025/raw/L1/radianceH5/*.h5')
-for fp in fps:
-    with h5py.File(fp, "r") as f:
-        time = f['CRBU/Radiance'].attrs['Acquisition_Time']
-        cloud_condition = f['CRBU/Radiance/RadianceDecimalPart'].attrs['Cloud conditions']
-        cloud_type = f['CRBU/Radiance/RadianceDecimalPart'].attrs['Cloud type']
-    acquistion_date = time.split(',')[0]
-    acquistion_start_time = time.split(',')[1].replace('[Computer Time in sec]', '').strip()
-    granule_id = f'NIS01_{acquistion_date.replace("-", "")}_{acquistion_start_time}'
-    flightline_id = granule_id
-    cloud_condition = cloud_condition.split(' (')[0]
+# # 2025
+# granule_ids = []
+# campaign_name = 'Colorado Headwaters Ecological Spectroscopy Study'
+# sensor_name = 'NEON AIS 1'
+# acquistion_dates = []
+# acquistion_start_times = []
+# cloudy_conditions = []
+# cloud_types = []
+# gsd = 1
+# raster_epsg = 32613
+# flightline_ids = []
+# fps = glob('/store/carroll/col/data/2025/raw/L1/radianceH5/*.h5')
+# for fp in fps:
+#     with h5py.File(fp, "r") as f:
+#         time = f['CRBU/Radiance'].attrs['Acquisition_Time']
+#         cloud_condition = f['CRBU/Radiance/RadianceDecimalPart'].attrs['Cloud conditions']
+#         cloud_type = f['CRBU/Radiance/RadianceDecimalPart'].attrs['Cloud type']
+#     acquistion_date = time.split(',')[0]
+#     acquistion_start_time = time.split(',')[1].replace('[Computer Time in sec]', '').strip()
+#     granule_id = f'NIS01_{acquistion_date.replace("-", "")}_{acquistion_start_time}'
+#     flightline_id = granule_id
+#     cloud_condition = cloud_condition.split(' (')[0]
 
-    granule_ids.append(granule_id)
-    acquistion_dates.append(acquistion_date)
-    acquistion_start_times.append(acquistion_start_time)
-    cloudy_conditions.append(cloud_condition)
-    cloud_types.append(cloud_type)
-df = pd.DataFrame({
-    'granule_id': granule_ids, 
-    'campaign_name': campaign_name,
-    'sensor_name': sensor_name,
-    'acquisition_date': acquistion_dates,
-    'acquisition_start_time': acquistion_start_times,
-    'cloudy_conditions': cloudy_conditions,
-    'cloud_type': cloud_types,
-    'gsd': gsd,
-    'raster_epsg': raster_epsg,
-    'flightline_id': granule_ids,
-    'granule_rad_url': [None] * len(granule_ids),
-    'granule_refl_url': [None] * len(granule_ids)
-})
-df.to_csv('out/2025/granule_metadata.csv', index=False)
+#     granule_ids.append(granule_id)
+#     acquistion_dates.append(acquistion_date)
+#     acquistion_start_times.append(acquistion_start_time)
+#     cloudy_conditions.append(cloud_condition)
+#     cloud_types.append(cloud_type)
+# df = pd.DataFrame({
+#     'granule_id': granule_ids, 
+#     'campaign_name': campaign_name,
+#     'sensor_name': sensor_name,
+#     'acquisition_date': acquistion_dates,
+#     'acquisition_start_time': acquistion_start_times,
+#     'cloudy_conditions': cloudy_conditions,
+#     'cloud_type': cloud_types,
+#     'gsd': gsd,
+#     'raster_epsg': raster_epsg,
+#     'flightline_id': granule_ids,
+#     'granule_rad_url': [None] * len(granule_ids),
+#     'granule_refl_url': [None] * len(granule_ids)
+# })
+# df.to_csv('out/2025/granule_metadata.csv', index=False)
--- wavelengths.py
+++ wavelengths.py
@@ -2,10 +2,8 @@ import os
 import pandas as pd
 import h5py
 
-os.chdir('C:/Users/carroll/Documents/sbgplant')
-
 # 2018
-fp = r"C:\Users\carroll\Downloads\2018_FullSite_D13_2018_CRBU_1_L1_Spectrometer_RadianceH5_2018061214_NEON_D13_CRBU_DP1_20180612_154959_radiance.h5"
+fp = '/store/carroll/col/data/2018/raw/L1/radianceH5/2018061214/NEON_D13_CRBU_DP1_20180612_154959_radiance.h5'
 with h5py.File(fp, "r") as f:
     wavelength = f["/CRBU/Radiance/Metadata/Spectral_Data/Wavelength"][:]
     fwhm = f["/CRBU/Radiance/Metadata/Spectral_Data/FWHM"][:]
@@ -18,20 +16,20 @@ df = pd.DataFrame({
     'wavelength': wavelength,
     'fwhm': fwhm
 })
-df.to_csv('out/2018/wavelengths.csv', index=False)
+df.to_csv('/store/carroll/sbgplants/out/2018/wavelengths.csv', index=False)
 
-# 2025
-fp = r"C:\Users\carroll\Downloads\2025_FullSite_D13_2025_CRBU_2_L1_Spectrometer_RadianceH5_2025062714_NEON_D13_CRBU_DP1_L003-1_20250627_radiance.h5"
-with h5py.File(fp, "r") as f:
-    wavelength = f["/CRBU/Radiance/Metadata/Spectral_Data/Wavelength"][:]
-    fwhm = f["/CRBU/Radiance/Metadata/Spectral_Data/FWHM"][:]
-campaign_name = 'Colorado Headwaters Ecological Spectroscopy Study'
-sensor_name = 'NEON AIS 1'
-df = pd.DataFrame({
-    'campaign_name': campaign_name,
-    'sensor_name': sensor_name,
-    'band': range(len(wavelength)),
-    'wavelength': wavelength,
-    'fwhm': fwhm
-})
-df.to_csv('out/2025/wavelengths.csv', index=False)
+# # 2025
+# fp = r"C:\Users\carroll\Downloads\2025_FullSite_D13_2025_CRBU_2_L1_Spectrometer_RadianceH5_2025062714_NEON_D13_CRBU_DP1_L003-1_20250627_radiance.h5"
+# with h5py.File(fp, "r") as f:
+#     wavelength = f["/CRBU/Radiance/Metadata/Spectral_Data/Wavelength"][:]
+#     fwhm = f["/CRBU/Radiance/Metadata/Spectral_Data/FWHM"][:]
+# campaign_name = 'Colorado Headwaters Ecological Spectroscopy Study'
+# sensor_name = 'NEON AIS 1'
+# df = pd.DataFrame({
+#     'campaign_name': campaign_name,
+#     'sensor_name': sensor_name,
+#     'band': range(1, len(wavelength) + 1),
+#     'wavelength': wavelength,
+#     'fwhm': fwhm
+# })
+# df.to_csv('out/2025/wavelengths.csv', index=False)
